a2e6e486943cdf75b661cce268d92f1f1290878c
chunhua <li000847@algonquinlive.com>
2024-03-17 18:16:09 -0400

    Initial commit

---
+++ src/foodwastereductionsystem/dataTier/claimTable.sql
@@ -0,0 +1,9 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  lchca
+ * Created: Mar 17, 2024
+ */
+

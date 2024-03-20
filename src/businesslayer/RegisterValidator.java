afe9892884dfa0e4e5a6f07e54985c4700429c8b
chunhua <li000847@algonquinlive.com>
2024-03-20 11:43:25 -0400

    second commit: create classes

--- src/businesslayer/RegisterValidator.java
+++ src/businesslayer/RegisterValidator.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package foodwastereductionsystem.presentationTier;
+package businesslayer;
 
 /**
  *
  * @author lchca
  */
-public class indexView {
+public class RegisterValidator {
     
 }
--- src/model/Inventory.java
+++ src/model/Inventory.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package foodwastereductionsystem.businessTier;
+package model;
 
 /**
  *
  * @author lchca
  */
-public class retailer {
+public class Inventory {
     
 }
